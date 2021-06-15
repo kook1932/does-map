378acda8c9e94ebbfc6e2238b9fc73740a2ce126
jeongyong-han <jeongyong.han@does.kr>
2021-06-15 16:16:33 +0900

    Initial commit
    kakao map 생성, 마커 추가

---
+++ src/main/java/com/example/doesmap/DoesMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.doesmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoesMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoesMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/doesmap/DoesMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doesmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoesMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
